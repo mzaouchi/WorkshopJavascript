59b4dcc6dbcad45ee9a07078052a8d69f41529e0
mzaouchi <mzaouchi@gmail.com>
2023-05-31 14:18:58 +0100

    first commit

---
+++ Workshop.js
@@ -0,0 +1,150 @@
+// // var a = "Brahim"
+// // console.log(a)
+// // console.log(typeof (a))
+
+
+// // var m = 8.7
+// // console.log(m)
+// // console.log(typeof(m))
+
+// // var bool = true
+// // console.log(bool)
+// // console.log(typeof(bool))
+
+
+// // var t = [1,2,3,4]
+// // console.log(t)
+// // console.log(t[2])
+// // console.log(typeof(t))
+
+// // var user = {name : "Mohamed",age : 31,city:"Tastour"}
+// // console.log(user)
+// // console.log(typeof(user))
+// // console.log(user.city)
+// // console.log(Object.keys(user))
+// // console.log(Object.values(user))
+// // console.log(Object.entries(user))
+
+// // var a
+// // console.log(typeof(a))
+
+// // var a = null
+// // console.log(typeof(a))
+
+// var a = 5
+// var b = 2
+// // ++a
+// console.log(++a)
+
+// // // var res = a+b
+// // // console.log(res)
+// // // console.log(a+b)
+// // // console.log(a-b)
+// // // console.log(a*b)
+// // // console.log(a/b)
+// // // console.log(a%b)
+// // a++
+// // console.log(a)
+// // a = a +1
+// // console.log(a)
+// // a--
+// // console.log(a)
+// // a = a -1
+// // console.log(a)
+// // console.log(a**b)
+
+// var c = "5"
+
+// console.log(a==c)
+// // console.log(a===b)
+// console.log(a===c)
+// console.log(a != c)
+// console.log(a !== c)
+// console.log(a>=b)
+// console.log(typeof(a+c))
+
+// var a = 5
+// var b = 2
+// var c = 90
+// console.log(a>b && b>c)
+// console.log(a > b || b > c)
+
+
+// var age = 45
+
+
+// // if(age<18){
+// //   console.log("Enfant")
+// // }
+
+// if(age<= 18){
+//   console.log("Enfant")
+// }else if(age<=60){
+//   console.log("Adulte")
+// }else{
+//   console.log("Vieux")
+// }
+
+// var mois = 2
+
+
+// switch(mois){
+//   case 1 : console.log("Janvier")
+//            break
+//   case 2 : console.log("Fevrier")
+//            break
+//   default : console.log("Variable no valide")
+// }
+
+
+
+// function Somme(a,b){
+//   var res = a+b
+//   return res
+// }
+
+// console.log(Somme(2,8))
+// console.log(Somme(8, 8))
+// console.log(Somme(9, 8))
+// console.log(Somme("00", 8))
+
+
+// // JavaScript Loops
+
+// var tab = [1,2,3,4,5,6]
+
+// for(var i = 0;i<tab.length;i++){
+//   console.log(tab[i])
+// }
+
+// for (var key in tab){
+//   console.log(tab[key])
+// }
+
+// for(var mahmoud of tab){
+//   console.log(mahmoud)
+// }
+
+// While 
+
+// var tab = [1,2,3,4,5,6]
+
+// var i = 0
+
+// while(i<tab.length){
+//   console.log(tab[i])
+//   i++
+
+// }
+
+// do while
+
+// var tab = [1,2,3,4,5,6]
+
+// var i = 9
+
+// do{
+//   console.log(tab[i])
+//   i++
+// }while(i<tab.length)
+
